53a3a317df68ce929b5b7c092ed1371a2b304751
amsmeier <amsmeier@gmail.com>
2024-03-27 19:24:43 -0400

    .

--- compare_areal_tuning.m
+++ compare_areal_tuning.m
@@ -105,12 +105,13 @@ if show_barplot
     hyline = yline(alpha);
     set(0, 'DefaultTextInterpreter', 'none')
     titlestr = [full_param_string, '..... p = ' num2str(chi_p)] ;
-    htitle = title(titlestr); 
+%     htitle = title(titlestr); 
 
     hbar.FaceColor = bar_face_color; 
 
 end
 
+box off
 ylabel('fraction of electrodes tuned')
 
 hold off
