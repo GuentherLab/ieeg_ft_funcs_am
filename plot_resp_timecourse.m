dd565c3c7c0327e773bc16e6f2495fe8e038db8c
amsmeier <amsmeier@users.noreply.github.com>
2024-04-16 21:10:39 -0400

    .

--- plot_resp_timecourse.m
+++ plot_resp_timecourse.m
@@ -1,44 +1,26 @@
  %%%% plot the timecourse and SD of an electrode's response
- % load preprocessed trialwise data and run resp_types.m first
- %
- % align all trialwise responses to speech onset
- %
+ %%%% .... optionally sort trials by specific conditions 
+ %%%% .... responses will be time-locked to a particular timepoint (e.g. speech onset)
+ % 
+ % this script is intended by be called by project-specific wrapper scripts, e.g. plot_resp_timecourse_triplet
  
 %% params
 
 vardefault('srt_row', 1);
-
-vardefault('sort_by_trial_cond',0)
-
-% set(0,'DefaultFigureWindowStyle','docked')
-set(0,'DefaultFigureWindowStyle','normal')
-
 vardefault('plot_raster',0); 
-
 
 set_project_specific_variables()
 
-switch TASK
-    case 'smsl'
-        vardefault('sort_cond','learn_con');
-
-    case 'triplet'
-
-    otherwise 
-        error('unknown task')
-end
-
 channame = srt.chan{srt_row};
 thissub = srt.sub{srt_row};
-
-resprow = strcmp(resp.chan,channame) & strcmp(resp.sub,thissub);
  
+% remove trials with no response data
+non_empty_trials = ~cellfun(@isempty, timecourses_unaligned);
+trials_tmp = trials_tmp(non_empty_trials,:);
+timecourses_unaligned = timecourses_unaligned(non_empty_trials);
 
 
-
-%% align responses
- % align responses to first syl onset
-
+%% align responses using alignment times provided within trials_tmp by the wrapper function
 subind = string(subs.subject)==thissub; 
 
 ntrials = height(trials_tmp);
@@ -49,15 +31,12 @@ ntrials = height(trials_tmp);
  
  samp_period = 1e-5 * round(1e5 * diff(trials_tmp.times{1}(1:2))); % sampling interval
  
- %%% find trial lengths pre- and post-onset
-    %%%% trials.times{itrial} use global time coordinates
-    %%%% ....... start at a fixed baseline window before stim onset
-    %%%% ....... end at a fixed time buffer after speech offset
+ %%% find trial lengths pre- and post- the alignment time
 for itrial = 1:ntrials
-    % n timepoints before or at speech onset [time from beginning of pre-stim baseline to speech onset]
-    trials_tmp.tpoints_pre_onset(itrial) = nnz(trials_tmp.times{itrial} <= trials_tmp.t_prod_on(itrial,1)); 
-    % n timepoints after speech onset [time from voice onset to end of post-speech time buffer]
-    trials_tmp.tpoints_post_onset(itrial) = nnz(trials_tmp.times{itrial} > trials_tmp.t_prod_on(itrial,1)); 
+    % n timepoints before or at align_time
+    trials_tmp.tpoints_pre_onset(itrial) = nnz(trials_tmp.times{itrial} <= trials_tmp.align_time(itrial,1)); 
+    % n timepoints after align_time
+    trials_tmp.tpoints_post_onset(itrial) = nnz(trials_tmp.times{itrial} > trials_tmp.align_time(itrial,1)); 
 end
  
 % pad or cut each trial to fit a specific size, so that we can align and average trials
@@ -98,24 +77,19 @@ resp_align.n_nonnan_trials = sum(~isnan(resp_align.resp)); % number of usable tr
 resp_align.sem = resp_align.std ./ sqrt(resp_align.n_nonnan_trials);
 resp_align.sem_lims = [resp_align.mean + resp_align.sem; resp_align.mean - resp_align.sem]; 
 
-
-%% plotting
 xtime = 0.5 + [linspace(-n_tpoints_pre_fixed, -1, n_tpoints_pre_fixed), linspace(0, n_tpoints_post_fixed-1, n_tpoints_post_fixed)];
 xtime = samp_period * xtime; 
 
+%% plotting
+if newfig
+    hfig = figure('Color',[1 1 1]);
+end
 
-
-% close all
-    %     hfig = figure('Color',[1 1 1]);
-
-if sort_by_trial_cond
-    if isnumeric(unq_conds)
+if ~isempty(sort_cond)
+    [unq_conds, ~, trial_cond_ind] = unique( trial_conds );
+    if isnumeric(unq_conds) % remove NaN condition labels
         [unq_conds, ~, trial_cond_ind] = unq_conds(~isnan(unq_conds));
         unq_conds = cellstr(num2str(unq_conds));
-        trialconds  = strtrim(cellstr(num2str(trials_tmp{:,sort_cond})));
-    else
-        [unq_conds, ~, trial_cond_ind] = unique( trial_conds );
-        trialconds = trials_tmp{:,sort_cond}; 
     end
     nconds = length(unq_conds);
 
@@ -130,15 +104,14 @@ if sort_by_trial_cond
 
     % if grouping val indices not specified, plot them all
     if isempty (condval_inds_to_plot)
-        condval_inds_to_plot = 1:ncondvals;
+        condval_inds_to_plot = 1:nconds;
     end
     nvals_to_plot = length(condval_inds_to_plot); 
 
     % plot error bars
-    %%% plotting error bars separately from means makes it simpler to create legend
     for icond = 1:nconds
         thiscond = unq_conds{icond};
-        resp_rows_match = strcmp(trialconds, thiscond);
+        resp_rows_match = strcmp(trial_conds, thiscond);
         this_cond_resp = resp_align.resp(resp_rows_match,:); % aligned trial timecourses for trials that match this condition label
         this_cond_mean = nanmean(this_cond_resp);
          this_cond_std = std(this_cond_resp, 'omitnan'); % stdev of response timecourses
@@ -148,23 +121,20 @@ if sort_by_trial_cond
         plotinds = this_cond_n_nonnan_trials > 0; % timepoints with computable error bars
 
         if nnz(plotinds) > 0
-            hfill = fill([xtime(plotinds), fliplr(xtime(plotinds))], [this_cond_sem_lims(1,plotinds), fliplr(this_cond_sem_lims(2,plotinds))], [0.8 0.8 0.8]); % standard error
+            lowlims = this_cond_sem_lims(1,plotinds); 
+            uplims = fliplr(this_cond_sem_lims(2,plotinds));
+            if smooth_timecourses
+                lowlims = smoothdata(lowlims, 2, smooth_method, smooth_windowsize); 
+                uplims = smoothdata(uplims, 2, smooth_method, smooth_windowsize); 
+            end
+
+            hfill = fill([xtime(plotinds), fliplr(xtime(plotinds))], [lowlims,uplims], [0.8 0.8 0.8], 'HandleVisibility','off'); % standard error
             hfill.LineStyle = 'none'; % no border
     
             hfill.EdgeColor = [0.8 0.8 0.8]; 
        end
        hold on
     end
-
-
-    % % % % % % % % plot means.... code originally for dbsseq version of function
-    % % % % % % % for icond = 1:nconds
-    % % % % % % %     thiscond = unq_conds{icond};
-    % % % % % % %     resp_rows_match = strcmp(trialconds, thiscond);
-    % % % % % % %     this_cond_resp = resp_align.resp(resp_rows_match,:); % aligned trial timecourses for trials that match this condition label
-    % % % % % % %     this_cond_mean = nanmean(this_cond_resp);
-    % % % % % % %     hplot(icond) = plot(xtime, this_cond_mean);
-    % % % % % % % end
 
     timecourses_to_plot = cell2mat(resp_grpd.resp_mean(condval_inds_to_plot,:))';
     if smooth_timecourses
@@ -186,7 +156,7 @@ if sort_by_trial_cond
 
     legend_strs = [repmat({''},nconds,1); unq_conds]; % empty entries match error bars
 
-else
+elseif ~isempty(sort_cond)
 
 
     hold off
--- set_project_specific_variables.m
+++ set_project_specific_variables.m
@@ -9,48 +9,42 @@
 vardefault('op',struct);
 field_default('op','subj','DBS3012'); % triplet subject
 field_default('op','art_crit','E'); % default to high gamma 
-SUBJECT=op.sub;
-proj_str = regexprep(SUBJECT, '[0-9]', ''); % string that will tell us what project / collection site this subject is from
+proj_str = regexprep(op.sub, '[0-9]', ''); % string that will tell us what project / collection site this subject is from
 
 switch proj_str
     case 'DBS' % Pitt
         setpaths_dbs_triplet()
 
         TASK = 'triplet'; 
-
-        DATE=datestr(now,'yyyymmdd');
-        PATH_DATA='Z:\DBS';
-        PATH_SUBJECT=[PATH_DATA filesep SUBJECT];
+        PATH_SUBJECT=[PATH_DATA filesep op.sub];
         PATH_DER_SUB = [PATH_SUBJECT filesep 'Preprocessed Data']; % subject-specific derivatives
         PATH_SYNC = [PATH_DER_SUB filesep 'Sync'];
-        PATH_ART_PROTOCOL = ['Z:\DBS\Batch\P08_artifact_criteria_', op.art_crit];
         PATH_ANNOT = [PATH_SYNC '/annot']; 
         PATH_FIELDTRIP = [PATH_DER_SUB filesep 'FieldTrip'];
         
         % filepath and filename for saving artifact info into subject-specific annot folder
-        ARTIFACT_FILENAME_SUB = [PATH_ANNOT filesep SUBJECT '_artifact_criteria_', op.art_crit, '_not-denoised.txt']; 
+        ARTIFACT_FILENAME_SUB = [PATH_ANNOT filesep op.sub '_artifact_criteria_', op.art_crit, '_not-denoised.txt']; 
        
         % string (including filepath) at beginning of all fieldtrip filenames for this subj
-        FT_FILE_PREFIX = [PATH_FIELDTRIP, filesep, SUBJECT, '_ft_'];
+        FT_FILE_PREFIX = [PATH_FIELDTRIP, filesep, op.sub, '_ft_'];
 
         FT_RAW_FILENAME = [FT_FILE_PREFIX 'raw_session.mat']; 
         
-        artparam = readtable([PATH_ART_PROTOCOL, filesep, 'artifact_', op.art_crit , '_params.txt'],'FileType','text');
-        session= bml_annot_read([PATH_ANNOT filesep SUBJECT '_session.txt']);
-        electrodes = bml_annot_read([PATH_ANNOT filesep SUBJECT '_electrode.txt']);
+        session= bml_annot_read([PATH_ANNOT filesep op.sub '_session.txt']);
+        electrodes = bml_annot_read([PATH_ANNOT filesep op.sub '_electrode.txt']);
             electrodes.name = electrodes.electrode; % match the table variable name used in dbs-seq
         
         %%% trial_epoch variable might only be used in analyzing vibration-denoised data
         % DBS3031 is missing DBS3031_trial_epoch.txt in annot folder; instead only has DBS3031_trial_epoch_criteria_D.txt
-        if ~strcmp(SUBJECT, 'DBS3031') 
-            trial_epoch = readtable([PATH_ANNOT filesep SUBJECT '_trial_epoch.txt']);
-        elseif strcmp(SUBJECT, 'DBS3031') 
-            trial_epoch = readtable([PATH_ANNOT filesep SUBJECT '_trial_epoch_criteria_D.txt']);
+        if ~strcmp(op.sub, 'DBS3031') 
+            trial_epoch = readtable([PATH_ANNOT filesep op.sub '_trial_epoch.txt']);
+        elseif strcmp(op.sub, 'DBS3031') 
+            trial_epoch = readtable([PATH_ANNOT filesep op.sub '_trial_epoch_criteria_D.txt']);
         end
 
         % define trial epochs for referencing
         % % % Use generous buffers around the cue and speech production, even if this means that consecutive trials overlap. 
-        cue_presentation = bml_annot_read([PATH_ANNOT filesep SUBJECT '_cue_presentation.txt']);
+        cue_presentation = bml_annot_read([PATH_ANNOT filesep op.sub '_cue_presentation.txt']);
         epoch = cue_presentation(:,{'stim1_onset','ends','session_id','trial_id'});
         epoch.starts = epoch.stim1_onset - 1.5;
         epoch.ends = epoch.ends + 2;
@@ -62,7 +56,7 @@ switch proj_str
         SESSION = 'intraop';
         TASK = 'smsl'; 
         
-        PATH_DER_SUB = [PATH_DER filesep 'sub-' SUBJECT];  
+        PATH_DER_SUB = [PATH_DER filesep 'sub-' op.sub];  
         PATH_PREPROC = [PATH_DER_SUB filesep 'preproc'];
         PATH_ANNOT = [PATH_DER_SUB filesep 'annot'];
         PATH_FIELDTRIP = [PATH_DER_SUB filesep 'fieldtrip'];
@@ -73,28 +67,27 @@ switch proj_str
         PATH_TRIAL_AUDIO_INTRAOP_GO = [PATH_TRIAL_AUDIO filesep 'ses-', SESSION, '_go-trials'];
         PATH_TRIAL_AUDIO_INTRAOP_STOP = [PATH_TRIAL_AUDIO filesep 'ses-', SESSION, '_stop-trials']; 
         
-        PATH_SRC_SUB = [PATH_SRC filesep 'sub-' SUBJECT];  
+        PATH_SRC_SUB = [PATH_SRC filesep 'sub-' op.sub];  
         PATH_SRC_SESS = [PATH_SRC_SUB filesep 'ses-' SESSION]; 
         PATH_AUDIO = [PATH_SRC_SESS filesep 'audio']; 
         PATHS_TASK = strcat(PATH_SRC_SUB,filesep,{'ses-training';'ses-preop';'ses-intraop'},filesep,'task');
 
         % filepath and filename for saving artifact info into subject-specific annot folder
-        ARTIFACT_FILENAME_SUB = [PATH_ANNOT filesep 'sub-' SUBJECT '_ses-' SESSION '_task-' TASK '_artifact-criteria-' ARTIFACT_CRIT '_not-denoised.tsv'];
+        ARTIFACT_FILENAME_SUB = [PATH_ANNOT filesep 'sub-' op.sub '_ses-' SESSION '_task-' TASK '_artifact-criteria-' ARTIFACT_CRIT '_not-denoised.tsv'];
 
         % string (including filepath) at beginning of all fieldtrip filenames for this subj
-        FT_FILE_PREFIX = [PATH_FIELDTRIP, filesep, 'sub-', SUBJECT, '_ses-' SESSION '_task-' TASK, '_ft-'];
+        FT_FILE_PREFIX = [PATH_FIELDTRIP, filesep, 'sub-', op.sub, '_ses-' SESSION '_task-' TASK, '_ft-'];
         
         FT_RAW_FILENAME = [FT_FILE_PREFIX 'raw.mat']; 
         
         PATH_ART_PROTOCOL = ['Y:\DBS\groupanalyses\task-smsl\A09_artifact_criteria_', ARTIFACT_CRIT];
         PATH_FIGURES = [PATH_ART_PROTOCOL filesep 'figures']; 
 
-        artparam = readtable([PATH_ART_PROTOCOL, filesep, 'artifact_', ARTIFACT_CRIT , '_params.tsv'],'FileType','text');
-        session= bml_annot_read_tsv([PATH_ANNOT filesep 'sub-' SUBJECT '_sessions.tsv']);
+        session= bml_annot_read_tsv([PATH_ANNOT filesep 'sub-' op.sub '_sessions.tsv']);
 
         % merge electrode info into one table
-        electrodes = bml_annot_read_tsv([PATH_ANNOT filesep 'sub-' SUBJECT '_electrodes.tsv']);
-        channels = bml_annot_read_tsv([PATH_ANNOT filesep 'sub-' SUBJECT '_ses-' SESSION '_channels.tsv']); %%%% for connector info
+        electrodes = bml_annot_read_tsv([PATH_ANNOT filesep 'sub-' op.sub '_electrodes.tsv']);
+        channels = bml_annot_read_tsv([PATH_ANNOT filesep 'sub-' op.sub '_ses-' SESSION '_channels.tsv']); %%%% for connector info
             channels.name = strrep(channels.name,'_Ll','_Lm'); % change name to match naming convention in electrodes table
         [~, ch_ind] = intersect(channels.name, electrodes.name,'stable');
         electrodes = join(electrodes,channels(ch_ind,{'name','connector'}) ,'keys','name'); %%% add connector info
@@ -102,7 +95,7 @@ switch proj_str
         % define trial epochs for referencing
         % for dbs-seq/smsl, we will use experimenter keypress for trial start/end times
         %%%%% this means no trial overlap, but generally a large time buffer before cue onset and after speech offset
-        epoch = bml_annot_read_tsv([PATH_ANNOT filesep 'sub-' SUBJECT '_ses-' SESSION '_task-' TASK '_annot-trials.tsv']);
+        epoch = bml_annot_read_tsv([PATH_ANNOT filesep 'sub-' op.sub '_ses-' SESSION '_task-' TASK '_annot-trials.tsv']);
     otherwise
         error('Could not identify project from subject name')
 end
