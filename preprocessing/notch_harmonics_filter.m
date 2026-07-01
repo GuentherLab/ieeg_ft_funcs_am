33d28c921ad7c5735a15e32fab545c3769e2eb63
amsmeier <amsmeier@gmail.com>
2026-07-01 14:56:48 -0400

    preproc pipeline

--- preprocessing/notch_harmonics_filter.m
+++ preprocessing/notch_harmonics_filter.m
@@ -7,29 +7,25 @@ vardefault('cfg',struct);
 
 % modifiable parameters
 field_default('cfg','dftfreq',          [60 120 180 240 300 360 420 480]);
-field_default('cfg','dftbandwidth',     [1   1   1   1   1   1   1   1]);
-field_default('cfg','dftneighbourwidth',[2   2   2   2   2   2   2   2]);
+field_default('cfg','notch_bandwidth_hz', 2); 
+% % % % % % field_default('cfg','dftbandwidth',     [1   1   1   1   1   1   1   1]);
+
+% % % % % % % % % % field_default('cfg','dftneighbourwidth',[2   2   2   2   2   2   2   2]);
 
 % fix the following parameters
 cfg.dftfilter='yes';
 
+% 2026-7-1 note: this section originally was dealing with problem of not always having complete 60hz cycles
+%%%% RD not sure if even necessary anymore
 D_annot = bml_raw2annot(D_in);
 D_annot.nSamples2 = round(floor(D_annot.nSamples .* cfg.dftfreq(1)./D_annot.Fs) .* D_annot.Fs./cfg.dftfreq(1));
 D_annot.nSamples2 = round(floor(D_annot.nSamples2 .* cfg.dftfreq(1)./D_annot.Fs) .* D_annot.Fs./cfg.dftfreq(1));
 D_annot.nSamples2 = D_annot.nSamples2(:,1);
-
-
-%% AM question  - does this still need to be here? should it be a conditional? 
-% D_annot.nSamples2 = 938640; % hardcoded edit for DM1037
-
-
 D_annot.ends = D_annot.starts + D_annot.nSamples2 ./ D_annot.Fs;
 
 cfg1=[];
 cfg1.epoch=D_annot;
 D1= bml_redefinetrial(cfg1,D_in);
-
-%% AM note: skipping selectdata step because this would be awkward to use when all chans are already plotted in gui, including audio
 
 % mask zeroes as nans
 cfg1=[];
@@ -45,9 +41,8 @@ D1 = bml_mask(cfg1, D1);
 F = cfg.dftfreq;
 N = D_annot.nSamples2;
 Fs = D_annot.Fs;
-BW = 2; % Bandwith of notch filter in Hz
 
-newF = F + reshape(find(abs(1/N*Fs*(0:N-1) - F(1))<BW/2)-1,[],1)/N*Fs - F(1);
+newF = F + reshape(find(abs(1/N*Fs*(0:N-1) - F(1))< cfg.notch_bandwidth_hz /2)-1,[],1)/N*Fs - F(1);
 newF = reshape(newF,1,[]);
 cfg.dftfreq = newF;
 
